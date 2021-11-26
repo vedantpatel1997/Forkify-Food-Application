39b2825a811da713a0a259d5dc68a0d428303161
Vedant Patel <vedantp9@gmail.com>
2021-11-26 12:01:58 +0530

    intial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "01c3b0a8-65ef-4093-8766-00db61a4ea0d";
+export const MODAL_CLOSE_SEC = 2.5;
