1d5305efc4d265c97b88d4fdfeaf184d994c5234
vedantpatel1997 <vedantp9@gmail.com>
2021-11-26 12:31:39 +0530

    change of API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = "01c3b0a8-65ef-4093-8766-00db61a4ea0d";
+export const KEY = "cf51ef5c-3756-4d26-876b-a89102eb597b";
 export const MODAL_CLOSE_SEC = 2.5;
